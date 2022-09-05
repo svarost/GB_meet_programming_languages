6a627a0aa9c233a2e2a155df10701d725e6be37b
vsvarost <vsvarost@gmail.com>
2022-09-05 13:16:02 +0300

    Lesson 2 Task 1 DONE

---
+++ Lesson2_Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число   
+// и на выходе показывает вторую цифру этого числа.
+
+// Ввод числа из консоли по приглашению
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывод приглашения
+    string strValue; // Объявление переменной для ввода строки
+    strValue = Console.ReadLine() ?? "0"; // Вводим строку с консоли (с консоли можно ввести только строку)
+    int value = int.Parse(strValue); // Преобразование строки в целое число
+    return value;
+}
+
+int value = 0;
+while (value < 100 | value > 999 ) {
+    value = Prompt("Введите трехзначное число: ");
+}
+
+int [] rez = new int[3];
+for (int i = 0; i < 2; i++) {
+    rez[i] = value % 10;
+    value = value / 10;
+}
+System.Console.WriteLine(rez[1]);
