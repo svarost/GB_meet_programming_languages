edc5785d8033d7beb15cf44d70b83b50749bbc48
vsvarost <vsvarost@gmail.com>
2022-09-05 14:42:39 +0300

    Lesson 2 Task 5* DONE

---
+++ Lesson2_Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая генерирует несколько случайных чисел,  
+// и в цикле проверяет, кратны ли эти числа предварительно введенному числу,
+// при кратности - цикл прерывается.
+// Введенное число 2.
+
+int[] numbers = new int[5];
+for (int i = 0; i < 5; i++)
+{
+    numbers[i] = new Random().Next(0, 100);
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        System.Console.WriteLine($"{numbers[i]} -> да");
+        break;
+    }     
+    else System.Console.WriteLine($"{numbers[i]} -> нет");
+}
