51605726f10866f78b6f72acd1aced763ab375c6
vsvarost <vsvarost@gmail.com>
2022-09-07 13:22:22 +0300

    Lesson 3 Task 3 Done

---
+++ Lesson3_Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// Ввод числа из консоли по приглашению
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывод приглашения
+    string strValue; // Объявление переменной для ввода строки
+    strValue = Console.ReadLine() ?? "0"; // Вводим строку с консоли (с консоли можно ввести только строку)
+    if (int.TryParse(strValue, out int value))
+    {
+        return value;
+    }
+    throw new Exception("Данное значение невозможно преобразовать в число");
+}
+
+// Проверка на палиндром
+bool IsPalindrome(string value)
+{
+    bool rez = true;
+    for (int i = 0; i < value.Length / 2; i++)
+    {
+        if (value[i] != value[value.Length - 1 - i])
+        {
+            rez = false;
+            break;
+        }
+    }
+    return rez;
+}
+
+int value = Prompt("Введите пятизначное число: ");
+if (value < 9999 | value > 99999) // Проверка на пятизночность
+{
+    System.Console.WriteLine("Введено непятизначное число.");
+}
+else
+{
+    if (IsPalindrome(value.ToString()))
+    {
+        System.Console.WriteLine($"{value} -> да.");
+    }
+    else
+    {
+        System.Console.WriteLine($"{value} -> нет.");
+    }
+}
