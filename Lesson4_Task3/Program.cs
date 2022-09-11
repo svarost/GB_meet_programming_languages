bed6484cecba6ba39a0f7139b5aa17c5f5ecffbc
vsvarost <vsvarost@gmail.com>
2022-09-11 22:31:27 +0300

    Lesson 4 Task 3 START

---
+++ Lesson4_Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+System.Console.WriteLine(SumDigitsOfNumber(Prompt("Введите массив из 8 элементов: ")));
+
+
+// Создание массива
+int[] CreatArray(string String)
+{   
+    пше
+
+
+    return array;
+}
+
+// Ввод числа из консоли по приглашению
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывод приглашения
+    // string strValue; // Объявление переменной для ввода строки
+    // strValue = Console.ReadLine(); // Вводим строку с консоли (с консоли можно ввести только строку)
+    if (int.TryParse(Console.ReadLine(), out int value))
+    {
+        return value;
+    }
+    throw new Exception("Данное значение невозможно преобразовать в число");
+}
