5330ff77366b54df2c03f1bffd70b1281a4c59bf
vsvarost <vsvarost@gmail.com>
2022-09-27 15:49:52 +0300

    Lesson 9 Task 1 Done

---
+++ Lesson9_Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+//Main
+int value = Promt("Введите натуральное чило N: ");
+Console.WriteLine($"N = {value} -> \"{NumbersRec(value)}\"");
+
+
+// Methods
+int Promt(string message)
+{
+    Console.WriteLine(message);
+    if (int.TryParse(Console.ReadLine(), out int value))
+    {
+        return value;
+    }
+    throw new Exception("Введено некорректное значение");
+}
+
+string NumbersRec(int value)
+{
+    if (value > 1) return $"{value}, " + NumbersRec(value - 1);
+    else return "1";
+}
